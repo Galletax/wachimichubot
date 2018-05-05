16148584fe9a232118995337ffcfb38f4b57d421
Galletax <39006790+Galletax@users.noreply.github.com>
2018-05-05 19:14:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.user.setGame('No ser calvo')
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
