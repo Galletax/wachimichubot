d3dd771a0cb1e5656a5263f6275902fa88a6e8b4
Galletax <39006790+Galletax@users.noreply.github.com>
2018-05-05 19:00:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ client.on('message', msg => {
 if (message.content == ',help2') {
         message.channel.sendMessage(',cringedance -- Gif de Wismichu bailado');
         message.channel.sendMessage(',achus -- Gif de Wismichu bailado');
+  }
+});
   
 client.on('message', msg => {
   if (msg.content === ',help') {
