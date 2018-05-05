f6e524e1ca99df7a8bb40704cb99472fcda8a27d
Galletax <39006790+Galletax@users.noreply.github.com>
2018-05-05 19:07:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.user.setPresence({ status: 'online', game: { name: 'No ser calvo' } });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
