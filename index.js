ee34afe03f6bf7f7a6725ae5d9a4280e26195f33
Galletax <39006790+Galletax@users.noreply.github.com>
2018-05-05 15:25:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === ',cringedance') {
+    msg.reply('https://thumbs.gfycat.com/NeedyImmediateCusimanse-max-1mb.gif');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === ',achus') {
+    msg.reply('https://cdn.discordapp.com/attachments/442084669433839618/442302673635770368/achus.jpg');
+  }
+});
+
+
+
+client.login('NDQyMjgwMTE4MzUwNzc0Mjgy.Dc8g-Q.H_6vTlc9wjqT-7myd6JTm4Y6BrI');
